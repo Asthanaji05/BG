6467d4181771e164d4b89a39163b8eef73d71107
Asthanaji05 <shashank.asthana05@gmail.com>
2025-02-19 22:34:33 +0530

    audio bug fixing

--- src/pages/RightPage.jsx
+++ src/pages/RightPage.jsx
@@ -7,7 +7,7 @@ const RightPage = ({ commentary }) => {
 
     try {
       //const response = await fetch("https://bg-backend-ykf0.onrender.com/generate-audio", {
-        const response = await fetch("http://127.0.0.1:5000/generate-audio", {
+        const response = await fetch("https://bg-backend-ykf0.onrender.com/generate-audio", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text }),
